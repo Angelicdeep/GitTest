54691d14d123c330ca6f2486e8b438f921cb5236
志强 <1785231997@qq.com>
2026-05-24 20:25:31 +0800

    红点系统

---
+++ RedDotSystem/RedNode.cs
@@ -0,0 +1,13 @@
+public class RedNode
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Type { get; set; }
+    public string Value { get; set; }
+    public string Color { get; set; }
+    public string Icon { get; set; }
+    public string Image { get; set; }
+    public string Link { get; set; }
+    public string Target { get; set; }
+    public string Rel { get; set; }
+}
