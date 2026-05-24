9ed482a3f1336ef1154bd0fcfc6b7cc4ad0bbcae
志强 <1785231997@qq.com>
2026-05-24 19:59:35 +0800

    配置表类

---
+++ Config.cs
@@ -0,0 +1,11 @@
+public class Config
+{
+    public string Token { get; set; }
+    public string Prefix { get; set; }
+    public string Status { get; set; }
+    public string Activity { get; set; }
+    public string ActivityType { get; set; }
+    public string ActivityStatus { get; set; }
+    public string ActivityStatusType { get; set; }
+    public string ActivityStatusType { get; set; }  
+    }
